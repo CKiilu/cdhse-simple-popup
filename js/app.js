9e06f3ad004d86a049fd389443e734a99b1cb756
CKiilu <cndeti@gmail.com>
2016-02-22 08:24:48 +0300

    open popup jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+jQuery(document).ready(function ($) {
+    //    open popup
+    $('.cd-popup-trigger').on('click', function (event) {
+        event.preventDefault();
+        $('.cd-popup').addClass('.is-visible');
+    });
+
+})
